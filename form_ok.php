025b4d35194c19aa4cb12d2d47c77d378cef23a2
Hajin <22100228@handong.ac.kr>
2023-10-08 22:09:51 +0900

    update

--- form_ok.php
+++ form_ok.php
@@ -1,6 +1,16 @@
 
 <?php
-    $
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $uname = $_POST["uname"];
+        $title = $_POST["title"];
+        $author = $_POST["author"];
+        $type = $_POST["type"];
+        $report = $_POST["report"];
+        $fromdate = $_POST["fromdate"];
+
+        $data = "$uname, $title, $author, $type, $report, $fromdate\n";
+        file_put_contents("data.txt", $data, FILE_APPEND); // 데이터를 파일에 추가
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
